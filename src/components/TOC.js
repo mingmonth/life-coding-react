a0e87b23cabb132c13920c89a24139b11bb84a0b
yskim <yskim@huinno.com>
2021-06-01 15:05:44 +0900

    initial commit

---
+++ src/components/TOC.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class TOC extends Component {
+  render() {
+    const lists = [];
+    const data = this.props.data;
+    let i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            onClick={function (id, e) {
+              console.log(e);
+              e.preventDefault();
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
+          >
+            {data[i].title}
+          </a>
+        </li>
+      );
+      i = i + 1;
+    }
+    return (
+      <nav>
+        <ul>{lists}</ul>
+      </nav>
+    );
+  }
+}
+
+export default TOC;
